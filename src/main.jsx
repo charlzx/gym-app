4e8a294e1619b215a359f8e3980b951cbd3024d6
charlzx <charlesobuzor@outlook.com>
2025-06-23 00:26:16 +0100

    setup Hash router

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom';
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
+import App from './App.jsx';
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/gym-app/">
+    <HashRouter>
       <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
+    </HashRouter>
+  </StrictMode>
+);
--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import fs from 'fs'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/gym-app/',
   build: {
     outDir: 'dist',
   },
-
-  // Copy index.html to 404.html after build
-  closeBundle() {
-    const distPath = path.resolve(__dirname, 'dist')
-    const indexFile = path.join(distPath, 'index.html')
-    const notFoundFile = path.join(distPath, '404.html')
-
-    if (fs.existsSync(indexFile)) {
-      fs.copyFileSync(indexFile, notFoundFile)
-      console.log('✅ 404.html generated for GitHub Pages.')
-    } else {
-      console.warn('⚠️ index.html not found. Skipping 404.html generation.')
-    }
-  }
 })
